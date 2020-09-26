5178cf70693ac63c88dbf6b5a46be5de10506548
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2020-09-26 14:56:24 +0600

    inital commit

---
+++ lib/Screens/category_meals_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:resturant/widget/meal_item.dart';
+import '../dummy_data.dart';
+import '../models/meal.dart';
+
+class CategoryMeals extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final String categoryId = routeArgs['id'];
+    final String categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: ListView.builder(
+            itemCount: categoryMeals.length,
+            itemBuilder: (ctx, index) {
+              return MealItem(
+                id: categoryMeals[index].id,
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                complexity: categoryMeals[index].complexity,
+                affordability: categoryMeals[index].affordability,
+              );
+            }));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import './Screens/categories_screen.dart';
+import './Screens/category_meals_screen.dart';
+import './Screens/meals_details_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 245, 222, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            body1: TextStyle(
+              color: Color.fromARGB(255, 12, 12, 12),
+            ),
+            body2: TextStyle(
+              color: Color.fromARGB(222, 11, 11, 12),
+            ),
+            title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold)),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+
+      /// `We can insitiate in route`
+      // home: CategoriesScreen(),
+      // for nevigations
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMeals.routeName: (ctx) => CategoryMeals(),
+        MealDetailsScreen.routeName: (ctx) => MealDetailsScreen(),
+      },
+      // this can bes used to handle complex structure
+      onGenerateRoute: (settings) {
+        
+      },
+
+      // it calls before failer of creating page/screen
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (context) => CategoriesScreen());
+      },
+    );
+  }
+}
